a9316123732d3942789ac98a4dc4797bdc2be817
jesus73514145 <jesus_soria@usmp.pe>
2024-08-22 09:29:52 -0500

    HU_TP_76_v1

--- s3-mf-manager/src/App.jsx
+++ s3-mf-manager/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Sedes from './pages/Sedes'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/sedes" element={<Sedes />} />
       </Routes>
     </Router>
   )
---
+++ s3-mf-manager/src/pages/Sedes.jsx
@@ -0,0 +1,7 @@
+export default function Sedes(){
+    return(
+        <main className="100vh">
+            <h1>hola sedes</h1>
+        </main>
+    )
+}
