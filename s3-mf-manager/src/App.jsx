858d18599c1338d88df10adf92283cf50e4e6874
Razet97 <brandon_huanca@usmp.pe>
2024-08-27 01:35:00 -0500

    HU_TP_67_v2

--- s3-mf-manager/src/App.jsx
+++ s3-mf-manager/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'; // Asegúrate de que la ruta sea corre
 import Footer from './components/Footer'; // Asegúrate de que la ruta sea correcta
 import Tipodeproducto from './components/Tipodeproducto'; // Asegúrate de que la ruta sea correcta
 import ActualizarInventarioSedes from './components/ActualizarInventarioSedes'; // Asegúrate de que la ruta sea correcta
+import GenerarNuevoInventario from './components/GenerarNuevoInventario';
 import './App.css'; // Asegúrate de que la ruta sea correcta
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/GenerarInventario" element={<Tipodeproducto />} />
           <Route path="/ActualizarInventarioSedes" element={<ActualizarInventarioSedes />} />
+          <Route path="/GenerarNuevoInventario" element={<GenerarNuevoInventario />} />
         </Routes>
         <Footer />
       </>
--- s3-mf-manager/src/components/ActualizarInventarioSedes.jsx
+++ s3-mf-manager/src/components/ActualizarInventarioSedes.jsx
@@ -1,14 +1,80 @@
-import React from 'react';
-import './styles/ActualizarInventarioSedes.css'; // Asegúrate de que la ruta sea correcta
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles/ActualizarInventarioSedes.css'; 
+import Modal from './Modal';
+import GenerarNuevoInventario from './GenerarNuevoInventario'; // Asegúrate de que la ruta sea correcta
 
 const ActualizarInventarioSedes = () => {
-  return (
-    <div className="product-selection-container">
-      <a href="#" className="back-link"> ← Regresar</a>
-      <h2 className="main-title">Primero debes seleccionar un Tipo de Producto</h2>
-      
-    </div>
-  );
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const navigate = useNavigate();
+
+    const handleRegisterNewInventory = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div className="inventory-container">
+            <div className="header-container">
+                <a href="#" className="back-link">⟵ Regresar</a>
+                <div className="title-container">
+                    <h1 className="main-title">Registra las últimas actualizaciones</h1>
+                    <h1 className="main-title-2">de los inventarios de tus sedes!</h1>
+                </div>
+                <div className="action-buttons">
+                    <button className="button-class" onClick={handleRegisterNewInventory}>+ Registrar Nuevo Inventario</button>
+                    <input type="text" placeholder="Buscar" className="input-class" />
+                </div>
+            </div>
+            <table className="inventory-table">
+                <thead>
+                    <tr className="table-header">
+                        <th className="cell-class">Asignar</th>
+                        <th className="cell-class">Imagen del producto</th>
+                        <th className="cell-class">Nombre</th>
+                        <th className="cell-class">Cantidad</th>
+                        <th className="cell-class">Precio de Compra</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="table-row">
+                        <td className="cell-class"><input type="checkbox" /></td>
+                        <td className="cell-class"><img src="https://placehold.co/50x50" alt="Mancuernas 3kg" /></td>
+                        <td className="cell-class">Mancuernas 3kg</td>
+                        <td className="cell-class">2</td>
+                        <td className="cell-class">40 soles</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="cell-class"><input type="checkbox" /></td>
+                        <td className="cell-class"><img src="https://placehold.co/50x50" alt="Mancuerna 20kg" /></td>
+                        <td className="cell-class">Mancuerna 20kg</td>
+                        <td className="cell-class">3</td>
+                        <td className="cell-class">80 soles</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="cell-class"><input type="checkbox" /></td>
+                        <td className="cell-class"><img src="https://placehold.co/50x50" alt="Pack gimnasio en casa" /></td>
+                        <td className="cell-class">Pack gimnasio en casa</td>
+                        <td className="cell-class">5</td>
+                        <td className="cell-class">135 soles</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="cell-class"><input type="checkbox" /></td>
+                        <td className="cell-class"><img src="https://placehold.co/50x50" alt="Rueda abdominal" /></td>
+                        <td className="cell-class">Rueda abdominal</td>
+                        <td className="cell-class">8</td>
+                        <td className="cell-class">25 soles</td>
+                    </tr>
+                </tbody>
+            </table>
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                <GenerarNuevoInventario />
+            </Modal>
+        </div>
+    );
 };
 
 export default ActualizarInventarioSedes;
