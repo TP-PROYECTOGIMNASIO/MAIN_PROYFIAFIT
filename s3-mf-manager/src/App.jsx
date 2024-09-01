fc7d9f2e829884b7c01025ea3175ec8681fad800
LucasEspLau <lukaespinoza12@gmail.com>
2024-09-01 17:48:37 -0500

    main_v9

--- s3-mf-manager/src/App.jsx
+++ s3-mf-manager/src/App.jsx
@@ -15,7 +15,7 @@ import InformePrincipal from "./pages/visualizarInforme/InformePrincipal";
 import Tipodeproducto from "./pages/inventario/Tipodeproducto";
 import ActualizarInventarioSedes from "./pages/inventario/ActualizarInventarioSedes";
 import GenerarNuevoInventario from "./pages/inventario/GenerarNuevoInventario";
-import MembresiaPage from './pages/membresias/MembershipPage';
+import MembershipPage from './pages/membresias/MemberShipPage';
 
 
 function App() {
@@ -75,7 +75,7 @@ function App() {
       <Route path="/membresias" element={
         <>
           <Navbar />
-          <MembresiaPage />
+          <MembershipPage />
           <Footer/>
         </>
 
