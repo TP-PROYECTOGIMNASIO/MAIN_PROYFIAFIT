a63140c57c568c735d0246ce0e8a2bf5c8b07b84
Pedro Nalvarte Montes <128196210+PedroNalvarte@users.noreply.github.com>
2024-09-19 09:38:09 -0700

    Delete CLONE_BUILD directory

--- CLONE_BUILD/s3-mf-clientes/src/components/shopping-cart/empty-shopping-cart.jsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyShoppingCart() {
-  return (
-    <div className="bg-white p-4 rounded-lg border shadow-lg">
-      <h5 className="text-lg font-medium border-b">Tu carrito está vacío.</h5>
-    </div>
-  );
-}
--- CLONE_BUILD/s3-mf-principal/dashboard/index.js
+++
@@ -1,3 +0,0 @@
-import DashboardScreen from './components/DashboardScreen';
-
-export { DashboardScreen };
