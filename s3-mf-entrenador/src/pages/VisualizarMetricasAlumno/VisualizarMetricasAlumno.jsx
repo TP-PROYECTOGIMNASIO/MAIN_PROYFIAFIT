c645a71ac6d38030676bef14d0e82d7ee5b9be09
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2024-09-20 04:06:07 +0000

    Sync from FRONTEND_FIAFIT

--- s3-mf-entrenador/src/pages/VisualizarMetricasAlumno/VisualizarMetricasAlumno.jsx
+++ s3-mf-entrenador/src/pages/VisualizarMetricasAlumno/VisualizarMetricasAlumno.jsx
@@ -1,48 +1,129 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
-import { FaArrowLeft as ArrowLeftIcon } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import { FaChevronDown as ChevronDownIcon, FaUser as UserIcon } from 'react-icons/fa';
 
-const VisualizarMetricas = () => {
+const ListaAlumnos = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [expandedIndex, setExpandedIndex] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedAlumno, setSelectedAlumno] = useState(null);
+    const [alumnos, setAlumnos] = useState([]);
 
-  
-  
+    // Fetch data from API
+    useEffect(() => {
+        const fetchAlumnos = async () => {
+            try {
+                const response = await fetch("https://3zn8rhvzul.execute-api.us-east-2.amazonaws.com/api/metricas-alumno/hu-tp-23/alumno?staff_id=11");
+                const data = await response.json();
+                setAlumnos(data);
+            } catch (error) {
+                console.error("Error fetching alumnos:", error);
+            }
+        };
+
+        fetchAlumnos();
+    }, []);
+
+    const filteredAlumnos = alumnos.filter(alumno =>
+        alumno.alumno.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    const handleExpand = (index) => {
+        setSelectedAlumno(filteredAlumnos[index]);
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedAlumno(null);
+    };
+
     return (
-      <div className="flex flex-col items-center w-full min-h-screen p-4 sm:p-6 bg-gray-100">
-        <main className="flex flex-col w-full p-6 sm:p-8 bg-white shadow-lg mt-6 max-w-4xl sm:max-w-5xl mx-auto rounded-lg">
-          <div className="flex items-center justify-between w-full mb-4 sm:mb-6">
-            <Link to="/registrar-metricas"> {/*solo cambia por el valor que desea regresar, debes de agregar el path que esta en APP.JSX*/}
-              <ArrowLeftIcon className="w-6 h-6 sm:w-8 sm:h-8 text-gray-600" />
-            </Link>
-            <h1 className="bg-red-600 text-white text-sm sm:text-base px-3 sm:px-4 py-1 sm:py-2 rounded-lg hover:bg-red-700 transition duration-300">MARZO</h1>
-          </div>
-          <div className="text-black w-full mb-4 sm:mb-6">
-            <p className="text-lg sm:text-2xl font-semibold text-left">Estas visualizando las métricas de ....</p>
-          </div>
-          <div className="w-full p-4 sm:p-6 mb-4 sm:mb-6 bg-gray-50 grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8">
-            <div className="text-black space-y-1 sm:space-y-2">
-              <p className="text-base sm:text-xl"><span className="font-semibold">Altura:</span> 178cm</p>
-              <p className="text-base sm:text-xl"><span className="font-semibold">Peso:</span> 85kg</p>
-              <p className="text-base sm:text-xl"><span className="font-semibold">Bicep derecho:</span> 45cm</p>
-              <p className="text-base sm:text-xl"><span className="font-semibold">Bicep izquierdo:</span> 46cm</p>
-            </div>
-            <div className="text-black space-y-1 sm:space-y-2">
-              <p className="text-base sm:text-xl"><span className="font-semibold">Cadera:</span> 68cm</p>
-              <p className="text-base sm:text-xl"><span className="font-semibold">Cintura:</span> 65cm</p>
-              <p className="text-base sm:text-xl"><span className="font-semibold">Muslo derecho:</span> 35cm</p>
-              <p className="text-base sm:text-xl"><span className="font-semibold">Muslo izquierdo:</span> 36cm</p>
-            </div>
-          </div>
-          <div className="text-black w-full p-4 sm:p-6 mb-4 sm:mb-6 bg-gray-50">
-            <p className="text-base sm:text-xl text-left"><span className="font-semibold">Objetivo:</span> Tonificar</p>
-            <p className="text-base sm:text-xl text-left"><span className="font-semibold">Peso Ideal:</span> 75Kg.</p>
-          </div>
-          <p className="text-xs sm:text-sm text-black text-right mb-4 sm:mb-6">Generado el 04-04-2024</p>
-          <button className="bg-gray-700 text-white px-4 py-2 rounded"> 
-            ACTUALIZAR MÉTRICAS
-          </button>
-        </main>
-      </div>
+        <div className="flex flex-col items-center w-full min-h-screen p-4 bg-gray-100">
+            <main className="flex flex-col items-center w-full p-4 bg-white shadow-md mt-4">
+                <h2 className="text-red-600 text-xl font-bold mb-4">Listado de Alumnos</h2>
+                <div className="w-full p-4 mb-4 bg-gray-50">
+                    <div className="flex justify-end mb-4">
+                        <div className="flex items-center">
+                            <input
+                                type="text"
+                                placeholder="Buscar por nombre"
+                                className="p-2 border border-gray-300 rounded-l-md w-40 sm:w-32 md:w-40 text-black"
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className="overflow-x-auto">
+                        <table className="w-full bg-white border border-gray-200 rounded-md shadow-md">
+                            <thead className="bg-gray-100">
+                                <tr className='text-black'>
+                                    <th className="p-2 border-b text-center">Nombre</th>
+                                    <th className="p-2 border-b text-center">Entrenador</th>
+                                    <th className="p-2 border-b text-center">Sede</th>
+                                    <th className="p-2 border-b text-center">Membresía</th>
+                                    <th className="p-2 border-b text-center">Detalles</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredAlumnos.map((alumno, index) => (
+                                    <tr key={alumno.idusuario} className='text-black'>
+                                        <td className="p-2 border-b text-center">{alumno.alumno}</td>
+                                        <td className="p-2 border-b text-center">{alumno.entrenador}</td>
+                                        <td className="p-2 border-b text-center">{alumno.sede}</td>
+                                        <td className="p-2 border-b text-center">{alumno.membership}</td>
+                                        <td className="p-2 border-b text-center">
+                                            <button
+                                                className="bg-gray-700 text-white px-3 py-1 rounded w-32"
+                                                onClick={() => handleExpand(index)}
+                                            >
+                                                {expandedIndex === index ? "Colapsar" : "Expandir"}
+                                                <ChevronDownIcon className={`inline-block ml-2 transform ${expandedIndex === index ? 'rotate-180' : ''}`} />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </main>
+
+            {/* Modal */}
+            {showModal && selectedAlumno && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-4">
+                    <div className="bg-white rounded-lg shadow-lg w-full max-w-sm md:max-w-md lg:max-w-lg p-4 relative">
+                        <button className="absolute top-2 right-2 text-gray-600" onClick={closeModal}>
+                            <ChevronDownIcon className="w-6 h-6" />
+                        </button>
+                        <div className="flex flex-col md:flex-row mb-4">
+                            <div className="flex-none w-full md:w-32 h-32 border-4 border-red-600 rounded-lg mb-4 md:mb-0 flex items-center justify-center">
+                                <UserIcon className="text-red-600 w-16 h-16" />
+                            </div>
+                            <div className="text-black flex-1 md:ml-4">
+                                <h3 className="text-lg font-bold mb-2">Detalles de {selectedAlumno.alumno}</h3>
+                                <p><strong>Nombre:</strong> {selectedAlumno.alumno}</p>
+                                <p><strong>Entrenador:</strong> {selectedAlumno.entrenador}</p>
+                                <p><strong>Sede:</strong> {selectedAlumno.sede}</p>
+                                <p><strong>Membresía:</strong> {selectedAlumno.membership}</p>
+                            </div>
+
+                        </div>
+                        <div className="flex flex-col md:flex-row gap-4">
+                            <div className="flex-1 flex flex-col gap-2 mb-4 md:mb-0">
+                                <button className="bg-red-600 text-white px-4 py-2 rounded text-sm">Horario</button>
+                                <button className="bg-red-600 text-white px-4 py-2 rounded text-sm"><a href='http://localhost:5173/alumno-checkin'> Métricas</a></button>
+                            </div>
+                            <div className="flex-1 flex flex-col gap-2">
+                                <button className="bg-gray-700 text-white px-4 py-2 rounded text-sm">PLAN ALIMENTICIO</button>
+                                <button className="bg-gray-700 text-white px-4 py-2 rounded text-sm">PLAN DE ENTRENAMIENTO</button>
+                                <button className="bg-gray-700 text-white px-4 py-2 rounded text-sm">REPORTE DE FISIOTERAPEUTA</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
-export default VisualizarMetricas;
+export default ListaAlumnos;
