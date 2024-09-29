b574f7207a6e0d34b79fefa2e9aecc81c8be17d1
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2024-09-29 04:19:50 +0000

    Sync from FRONTEND_FIAFIT

---
+++ s3-mf-administrador/src/components/AddEjercicioButton.jsx
@@ -0,0 +1,12 @@
+// src/components/AddEjercicioButton.jsx
+import React from 'react';
+
+const AddEjercicioButton = () => {
+  return (
+    <button className="add-ejercicio-button">
+      + Registrar Nuevo Ejercicio
+    </button>
+  );
+};
+
+export default AddEjercicioButton;
