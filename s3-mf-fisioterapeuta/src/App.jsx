802c68d55425418e6c975bfa12de23f61adddefb
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2024-10-10 04:44:23 +0000

    Sync from FRONTEND_FIAFIT

--- s3-mf-fisioterapeuta/src/App.jsx
+++ s3-mf-fisioterapeuta/src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import './index.css'
 
-
-import PhysicPage from './components/PhysicPage';
+import HUVISUALLIZARINICIOSEGN from './pages/HUVISUALLIZARINICIOSEGN';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
+
   return (
-    <div>
-      <PhysicPage />
-    </div>
-  );
+    <>
+      <Router>
+        <>
+        <Navbar/>
+        <Routes>
+          <Route  path="/" element={<HUVISUALLIZARINICIOSEGN />} />
+
+        </Routes>
+        <Footer/>
+        </>
+
+      </Router>
+
+    </>
+  )
 }
 
-export default App;
+export default App
