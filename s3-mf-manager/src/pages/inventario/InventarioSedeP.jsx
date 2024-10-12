f93664a118b7be7d171ef05e9fb122e612edc64e
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2024-10-12 04:00:25 +0000

    Sync from FRONTEND_FIAFIT

---
+++ s3-mf-manager/src/pages/inventario/InventarioSedeP.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
+import SeleccionarSedeI from "../../components/InventarioSede/SeleccionarSedeI";
+export default function InventarioSedeP() {
+  return (
+    <>
+      <Navbar />
+      <SeleccionarSedeI />
+      <Footer />
+    </>
+  );
+}
