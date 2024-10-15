47255037ab5894ece082315270ee14fca73c5764
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2024-10-15 17:24:07 +0000

    Sync from FRONTEND_FIAFIT

--- s3-mf-manager/src/pages/visualizarInforme/InformePrincipal.jsx
+++ s3-mf-manager/src/pages/visualizarInforme/InformePrincipal.jsx
@@ -1,13 +1,12 @@
-import Footer from "../../components/Footer";
-import Navbar from "../../components/Navbar";
+
 import "./InformePrincipal.css";
 import VisualizarInforme from "../../components/VisualizarInforme/VisualizarInforme"
 export default function InformePrincipal() {
   return (
     <>
-      <Navbar />
+ 
       <VisualizarInforme />
-      <Footer />
+  
     </>
   );
 }
