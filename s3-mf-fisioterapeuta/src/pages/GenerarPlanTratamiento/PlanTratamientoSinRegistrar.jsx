df4169be81ec1d46a03502e899be3870d43d853c
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2024-10-17 04:46:20 +0000

    Sync from FRONTEND_FIAFIT

---
+++ s3-mf-fisioterapeuta/src/pages/GenerarPlanTratamiento/PlanTratamientoSinRegistrar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const PlanTratamientoSinRegistrar = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      {/* Botón de Regresar */}
+      <header className="p-4">
+        <button className="text-gray-600 font-semibold text-sm">{'< Regresar'}</button>
+      </header>
+
+      {/* Contenido Principal */}
+      <main className="flex-grow flex justify-center items-center">
+        {/* Contenedor Blanco */}
+        <div className="bg-white p-10 rounded-lg shadow-lg text-center w-full max-w-lg space-y-4">
+          <h1 className="text-red-700 font-semibold text-xl">PLAN DE TRATAMIENTO</h1>
+          <p className="text-gray-500 text-lg">Nombre del Alumno</p>
+          <p className="text-gray-500 text-base mt-8">
+            Aún no se ha registrado Plan Tratamiento
+          </p>
+          <button className="bg-red-700 text-white py-2 px-4 rounded-lg mt-6 hover:bg-red-800 transition duration-300">
+            GENERAR PLAN TRATAMIENTO
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default PlanTratamientoSinRegistrar;
